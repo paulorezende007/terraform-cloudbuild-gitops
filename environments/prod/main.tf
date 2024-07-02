be23d352655eef81c54d94cccda054dc27750915
Paulo Rezende <47573710+paulorezende007@users.noreply.github.com>
2024-07-02 12:26:55 -0300

    Update main.tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 
-# module "vpc" {
-#   source  = "../../modules/vpc"
-#   project = "${var.project}"
-#   env     = "${var.env}"
-#   depends_on = [google_project_service.gcp_services]
-# }
+module "vpc" {
+  source  = "../../modules/vpc"
+  project = "${var.project}"
+  env     = "${var.env}"
+  depends_on = [google_project_service.gcp_services]
+}
 
 # module "http_server" {
 #   source  = "../../modules/http_server"
